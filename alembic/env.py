7646baca22cd1bdfef436043f9c5607b4a5c6782
rmusayevr <rmusayevr19@gmail.com>
2025-12-30 15:49:48 +0100

    Day 29: Add GitHub Actions CI pipeline

--- alembic/env.py
+++ alembic/env.py
@@ -10,7 +10,7 @@ from alembic import context
 from app.core.config import settings
 from app.db.base import Base
 from app.models.movie import MovieModel  # noqa: F401
-from app.models.user import UserModel    # noqa: F401
+from app.models.user import UserModel  # noqa: F401
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
