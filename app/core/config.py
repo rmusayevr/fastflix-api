e1490c1131c595c04a13b05dcec959eeffd2385d
rmusayevr <rmusayevr19@gmail.com>
2026-01-01 19:22:39 +0100

    Day 50: The Safety Net (Automated Testing)

--- app/core/config.py
+++ app/core/config.py
@@ -17,7 +17,6 @@ class Settings(BaseSettings):
     ALGORITHM: str = "HS256"
     ACCESS_TOKEN_EXPIRE_MINUTES: int = 30
 
-    REDIS_URL: str | None = None
     REDIS_HOST: str = "localhost"
     REDIS_PORT: int = 6379
     REDIS_DB: int = 0
