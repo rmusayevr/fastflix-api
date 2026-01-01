a93e3c5b31d25d28c73edbb806506c1e5a46eaae
rmusayevr <rmusayevr19@gmail.com>
2026-01-01 19:20:22 +0100

    Day 50: The Safety Net (Automated Testing)

--- app/core/redis.py
+++ app/core/redis.py
@@ -1,10 +1,17 @@
 import redis.asyncio as redis
 from app.core.config import settings
 
-# Create a connection pool for Redis
-redis_client = redis.from_url(
-    settings.REDIS_URL, encoding="utf-8", decode_responses=True
-)
+if settings.REDIS_URL:
+    redis_client = redis.from_url(
+        settings.REDIS_URL, encoding="utf-8", decode_responses=True
+    )
+else:
+    redis_client = redis.Redis(
+        host=settings.REDIS_HOST,
+        port=settings.REDIS_PORT,
+        encoding="utf-8",
+        decode_responses=True,
+    )
 
 
 async def get_redis():
