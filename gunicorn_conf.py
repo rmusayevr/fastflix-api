7bc51be5e4bb2d862fd01aaabcc72ab4dd6dcdc4
rmusayevr <rmusayevr19@gmail.com>
2026-01-02 17:38:28 +0100

    Day 52: Process Management

--- gunicorn_conf.py
+++ gunicorn_conf.py
@@ -1,13 +1,30 @@
+import multiprocessing
 import os
 
-port = os.getenv("PORT", "8000")
-bind = f"0.0.0.0:{port}"
+bind = os.getenv("BIND", "0.0.0.0:8000")
 
-workers = 2
+workers_str = os.getenv("WEB_CONCURRENCY", None)
+
+if workers_str:
+    workers = int(workers_str)
+else:
+    cores = multiprocessing.cpu_count()
+    workers_per_core = float(os.getenv("WORKERS_PER_CORE", "1"))
+    default_workers = int(workers_per_core * cores)
+
+    workers = min(default_workers, 4)
+
+if workers < 2:
+    workers = 2
+
 worker_class = "uvicorn.workers.UvicornWorker"
+
+max_requests = 1000
+max_requests_jitter = 50
 
 timeout = 120
 keepalive = 5
-loglevel = "info"
+
+loglevel = os.getenv("LOG_LEVEL", "info")
 errorlog = "-"
 accesslog = "-"
