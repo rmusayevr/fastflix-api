bba822754731f57d6b74e586944f616fd316b6cf
rmusayevr <rmusayevr19@gmail.com>
2026-01-05 12:44:10 +0100

    Day 68: Bulletproof Tasks (Retries & Resilience)

--- app/tasks/scheduled_tasks.py
+++ app/tasks/scheduled_tasks.py
@@ -1,13 +1,25 @@
 import time
+import random
 from app.core.celery_app import celery_app
 
 
-@celery_app.task(name="daily_report")
-def daily_report_task():
+@celery_app.task(
+    name="daily_report",
+    bind=True,
+    autoretry_for=(ConnectionError,),
+    retry_kwargs={'max_retries': 3},
+    retry_backoff=True
+)
+def daily_report_task(self):
     """
-    Simulates generating a daily report.
+    Simulates a flaky task that fails randomly.
     """
-    print("📈 STARTING DAILY REPORT GENERATION...")
+    print(f"📉 Attempt {self.request.retries}: Starting Daily Report...")
+    
+    if random.choice([True, False]):
+        print("❌ Network Glitch! Raising ConnectionError...")
+        raise ConnectionError("Database is unreachable!")
+        
     time.sleep(3)
-    print("✅ DAILY REPORT SENT!")
-    return "Report Generated Successfully"
+    print("✅ SUCCESS: Daily Report Generated!")
+    return "Report Generated"
